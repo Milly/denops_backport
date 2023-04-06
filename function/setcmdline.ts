a5a289f5104e5392e917f67fcbea8fd8bd84f69f
Milly <milly.ca@gmail.com>
2023-04-07 08:16:46 +0900

    fix: setcmdline can use in batch

--- function/setcmdline.ts
+++ function/setcmdline.ts
@@ -1,7 +1,37 @@
-import type { Denops } from "https://deno.land/x/denops_std@v3.12.0/mod.ts";
-import { exists } from "https://deno.land/x/denops_std@v3.12.0/function/common.ts";
+import { execute } from "https://deno.land/x/denops_std@v4.1.1/helper/execute.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v4.1.1/mod.ts";
+import { generateUniqueString } from "https://deno.land/x/denops_std@v4.1.1/util.ts";
 
-let setcmdline_exist: boolean | undefined;
+const cacheKey = Symbol("denops_backport/setcmdline");
+
+async function ensurePrerequisites(denops: Denops): Promise<string> {
+  if (typeof denops.context[cacheKey] === "string") {
+    return denops.context[cacheKey];
+  }
+  const suffix = generateUniqueString();
+  denops.context[cacheKey] = suffix;
+  const script = `
+  if exists('*setcmdline') ==# 1
+    function! DenopsBackport_setcmdline_${suffix}(...) abort
+      return call('setcmdline', a:000)
+    endfunction
+  else
+    function! DenopsBackport_setcmdline_${suffix}(str, ...) abort
+      if getcmdtype() ==# '' | return 1 | endif
+      let s = escape(a:str, '"\\')->substitute('[[:cntrl:]]', "\\<C-V>\\\\0", 'g')
+      if a:0 >= 1
+        let s = printf('["%s",setcmdpos(%d)][0]', s, a:1)
+      else
+        let s = printf('"%s"', s)
+      endif
+      call printf("\\<C-\\>e%s\\<CR>", s)->feedkeys('in')
+      return 0
+    endfunction
+  endif
+  `;
+  await execute(denops, script);
+  return suffix;
+}
 
 /**
  * Set the command line to {str} and set the cursor position to
@@ -9,46 +39,16 @@ let setcmdline_exist: boolean | undefined;
  * If {pos} is omitted, the cursor is positioned after the text.
  * Returns 0 when successful, 1 when not editing the command
  * line.
- *
- * **Note that the first call is not possible in a batch block.**
  */
 export async function setcmdline(
   denops: Denops,
   str: string,
   pos?: number,
 ): Promise<number> {
-  setcmdline_exist ??= await exists(denops, "*setcmdline");
-  if (setcmdline_exist === undefined) {
-    throw new Error("Can not be called in a batch block.");
-  }
-  return setcmdline_exist
-    ? setcmdline_native(denops, str, pos)
-    : setcmdline_fallback(denops, str, pos);
-}
-
-function setcmdline_native(
-  denops: Denops,
-  str: string,
-  pos?: number,
-): Promise<number>;
-function setcmdline_native(
-  denops: Denops,
-  ...args: unknown[]
-): Promise<number> {
-  return denops.call("setcmdline", ...args) as Promise<number>;
-}
-
-function setcmdline_fallback(
-  denops: Denops,
-  str: string,
-  pos?: number,
-): Promise<number> {
-  const exprStr =
-    `substitute(escape(l:str,'"\\'),'[[:cntrl:]]',"\\<C-V>\\\\0",'g')`;
-  const exprCmdlineReplace = (pos !== undefined)
-    ? `printf('["%s",setcmdpos(%d)][0]',${exprStr},l:pos)`
-    : `printf('"%s"',${exprStr})`;
-  const exprKeys = `printf("\\<C-\\>e%s\\<CR>",${exprCmdlineReplace})`;
-  const exprFeedkeys = `feedkeys(${exprKeys},'in')`;
-  return denops.eval(exprFeedkeys, { str, pos }) as Promise<number>;
+  const suffix = await ensurePrerequisites(denops);
+  return denops.call(
+    `DenopsBackport_setcmdline_${suffix}`,
+    str,
+    ...(typeof pos === "number" ? [pos] : []),
+  ) as Promise<number>;
 }
